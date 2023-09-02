aff7a20c856ad51d06e9cfeefe025f96b47d74a7
DennKK <karpovdenis.al@gmail.com>
2023-09-02 11:17:20 +0400

    "Two Sum" task

---
+++ src/easy/Solution.java
@@ -0,0 +1,17 @@
+package easy;
+import java.util.HashMap;
+
+// Two Sum
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int secondPart = target - nums[i];
+            if (map.containsKey(secondPart)) {
+                return new int[] {map.get(secondPart), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
