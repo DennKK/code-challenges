f674323c07b453c299e98544dcaa9f4fc3e2ebaa
DennKK <karpovdenis.al@gmail.com>
2023-09-24 23:06:06 +0300

    "BinarySearch" task

---
+++ src/easy/Solution1.java
@@ -0,0 +1,23 @@
+package easy;
+
+// BinarySearch
+class Solution1 {
+
+    public static int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int guess = nums[mid];
+            if (guess == target) {
+                return mid;
+            } else if (guess > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
