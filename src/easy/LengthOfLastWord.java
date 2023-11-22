d8993fed3f8b3ab6366a0c0fa9f93c6cece8941b
Denis Karpov <karpovdenis.al@gmail.com>
2023-11-22 11:06:29 +0300

    "Length of Last Word" task

---
+++ src/easy/LengthOfLastWord.java
@@ -0,0 +1,18 @@
+package easy;
+
+// Length of Last Word
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                if (length > 0) {
+                    return length;
+                }
+                continue;
+            }
+            length += 1;
+        }
+        return length;
+    }
+}
