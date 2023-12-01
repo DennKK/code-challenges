55273f2026e2defb591d1c3fc4c4ab18e65708c9
Denis Karpov <karpovdenis.al@gmail.com>
2023-12-01 13:23:17 +0300

    "Next Greater Element I" task

--- src/easy/NextGreaterElementI.java
+++ src/easy/NextGreaterElementI.java
@@ -36,11 +36,11 @@ public class NextGreaterElementI {
         Map<Integer, Integer> map = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < nums2.length; i++) {
-            while (!stack.isEmpty() && stack.peek() < nums2[i]) {
-                map.put(stack.pop(), nums2[i]);
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
             }
-            stack.push(nums2[i]);
+            stack.push(num);
         }
 
         for (int i = 0; i < nums1.length; i++) {
