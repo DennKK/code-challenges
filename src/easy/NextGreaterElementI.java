c26a2744b84b4db1b8a54e530a710c143544842c
Denis Karpov <karpovdenis.al@gmail.com>
2023-12-01 13:22:30 +0300

    "Next Greater Element I" task

---
+++ src/easy/NextGreaterElementI.java
@@ -0,0 +1,52 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+// Next Greater Element I
+public class NextGreaterElementI {
+    // First solution
+    public int[] nextGreaterElement1(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], i);
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = map.get(nums1[i]); j < nums2.length; j++) {
+                if (nums1[i] < nums2[j]) {
+                    ans[i] = nums2[j];
+                    break;
+                }
+            }
+
+            if (ans[i] == 0) {
+                ans[i] = -1;
+            }
+        }
+
+        return ans;
+    }
+
+    // Second solution
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < nums2.length; i++) {
+            while (!stack.isEmpty() && stack.peek() < nums2[i]) {
+                map.put(stack.pop(), nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return nums1;
+    }
+}
