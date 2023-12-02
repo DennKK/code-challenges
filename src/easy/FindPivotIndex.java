8b661475c628711794af48ebf082141b922e9a0e
Denis Karpov <karpovdenis.al@gmail.com>
2023-12-02 21:54:18 +0300

    "Find Pivot Index" task

---
+++ src/easy/FindPivotIndex.java
@@ -0,0 +1,26 @@
+package easy;
+
+// Find Pivot Index
+public class FindPivotIndex {
+    public int pivotIndex(int[] nums) {
+        int[] leftSum = new int[nums.length];
+        int ans = -1;
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            leftSum[i] = sum;
+            sum += nums[i];
+
+        }
+        sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum[nums.length - i - 1] == sum) {
+                ans = nums.length - i - 1;
+            }
+            sum += nums[nums.length - i - 1];
+        }
+
+        return ans;
+    }
+}
