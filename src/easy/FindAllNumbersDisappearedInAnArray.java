a077fb501eadfd7cd204daeec2fd82e2b798561a
Denis Karpov <karpovdenis.al@gmail.com>
2023-12-03 17:58:06 +0300

    "Find All Numbers Disappeared in an Array" task

---
+++ src/easy/FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,42 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Find All Numbers Disappeared in an Array
+public class FindAllNumbersDisappearedInAnArray {
+    // Fastest solution with extra memory
+    public List<Integer> findDisappearedNumbersFastest(int[] nums) {
+        int[] ans = new int[nums.length];
+        List<Integer> res = new ArrayList<>();
+
+        for (int num : nums) {
+            ans[num - 1] = 1;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (ans[i] != 1) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
+
+    // Follow-up solution
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[Math.abs(nums[i]) - 1] > 0) {
+                nums[Math.abs(nums[i]) - 1] *= -1;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
+}
