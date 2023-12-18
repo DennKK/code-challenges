1e4a88df8dec0ddbdc82dbd96146e5e070eda883
Denis Karpov <karpovdenis.al@gmail.com>
2023-12-18 12:37:38 +0300

    680. Valid Palindrome II

---
+++ src/easy/ValidPalindrome2.java
@@ -0,0 +1,30 @@
+package easy;
+
+// 680. Valid Palindrome II
+public class ValidPalindrome2 {
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left <= right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                String skipL = s.substring(left + 1, right + 1), skipR = s.substring(left, right);
+                return (isPalindrome(skipL) || isPalindrome(skipR));
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
