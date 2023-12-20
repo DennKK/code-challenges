66596d9cf184bcf756f3b30be9ab7004b92d5304
Denis Karpov <karpovdenis.al@gmail.com>
2023-12-20 12:02:55 +0300

    26. Remove Duplicates from Sorted Array

---
+++ src/easy/RemoveFromArray.java
@@ -0,0 +1,16 @@
+package easy;
+
+// 26. Remove Duplicates from Sorted Array
+public class RemoveFromArray {
+    public int removeDuplicates(int[] nums) {
+        int i = 1, left = 1;
+        while (i < nums.length) {
+            if (nums[i] == nums[i - 1]) {
+                i++;
+            } else {
+                nums[left++] = nums[i++];
+            }
+        }
+        return left;
+    }
+}
