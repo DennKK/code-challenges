ee1f1b0dc6f4936e1cb910c6aa916fcda90d3b0d
Denis Karpov <karpovdenis.al@gmail.com>
2024-01-04 16:34:52 +0300

    206. Reverse Linked List

---
+++ src/easy/RLL.java
@@ -0,0 +1,27 @@
+package easy;
+
+// 206. Reverse Linked List
+public class RLL {
+    static class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseListIteratively(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
