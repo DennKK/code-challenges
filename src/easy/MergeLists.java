92e11d94044f4cb4e27f8384357b3c6768e88b3c
Denis Karpov <karpovdenis.al@gmail.com>
2024-01-04 18:12:07 +0300

    21. Merge Two Sorted Lists

---
+++ src/easy/MergeLists.java
@@ -0,0 +1,41 @@
+package easy;
+
+// 21. Merge Two Sorted Lists
+public class MergeLists {
+    static private class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (l1 != null) {
+            tail.next = l1;
+        } else {
+            tail.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
