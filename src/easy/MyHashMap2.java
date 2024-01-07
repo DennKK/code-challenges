4277d4c8c2554ad240ed518866011a35f78ac02d
Denis Karpov <karpovdenis.al@gmail.com>
2024-01-07 18:36:19 +0300

    706. Design HashMap (second solution)

---
+++ src/easy/MyHashMap2.java
@@ -0,0 +1,25 @@
+package easy;
+
+import java.util.Arrays;
+
+// 706. Design HashMap
+// Second Solution
+public class MyHashMap2 {
+    private final int[] mp = new int[1000001];
+
+    public MyHashMap2() {
+        Arrays.fill(mp, -1);
+    }
+
+    public void put(int key, int value) {
+        mp[key] = value;
+    }
+
+    public int get(int key) {
+        return mp[key];
+    }
+
+    public void remove(int key) {
+        mp[key] = -1;
+    }
+}
