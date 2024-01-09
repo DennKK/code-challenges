77ce0a0ff7f816afd150eee42a46b8da546550ae
Denis Karpov <karpovdenis.al@gmail.com>
2024-01-09 12:10:46 +0300

    876. Middle of the Linked List

---
+++ src/easy/MiddleLinkedList.java
@@ -0,0 +1,30 @@
+package easy;
+
+// 876. Middle of the Linked List
+public class MiddleLinkedList {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    private ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
