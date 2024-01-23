ebd9e8c36a785d34ee71cb8d236d075125c800f5
Denis Karpov <karpovdenis.al@gmail.com>
2024-01-23 16:01:36 +0300

    33. Search in Rotated Sorted Array

---
+++ src/medium/SearchInRotatedSortedArray.java
@@ -0,0 +1,27 @@
+package medium;
+
+// 33. Search in Rotated Sorted Array
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[left] <= nums[mid]) {
+                if (target < nums[mid] && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
