8552911e1952292da08e8704c3420c8abb389334
Denis Karpov <karpovdenis.al@gmail.com>
2024-01-27 15:32:28 +0300

    3. Longest Substring Without Repeating Characters

---
+++ src/medium/LongestSubstring.java
@@ -0,0 +1,20 @@
+package medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// 3. Longest Substring Without Repeating Characters
+public class LongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        int res = 0, j = 0;
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            while (!set.add(s.charAt(i))) {
+                set.remove(s.charAt(j));
+                j++;
+            }
+            res = Math.max(res, set.size());
+        }
+        return res;
+    }
+}
