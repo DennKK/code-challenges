bab8d74c77b59df8e867cf5fc255cc2d07005c4d
Denis Karpov <karpovdenis.al@gmail.com>
2024-01-31 16:11:16 +0300

    94. Binary Tree Inorder Traversal (added iterative solution)

--- src/easy/BinaryTreeInorderTraversal.java
+++ src/easy/BinaryTreeInorderTraversal.java
@@ -2,6 +2,7 @@ package easy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 // 94. Binary Tree Inorder Traversal
 public class BinaryTreeInorderTraversal {
@@ -24,9 +25,29 @@ public class BinaryTreeInorderTraversal {
         }
     }
 
+    // Iterative solution
+    private List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+
+        return res;
+    }
+
+    // Recursive solution
     private final List<Integer> res = new ArrayList<>();
 
-    private List<Integer> inorderTraversal(TreeNode root) {
+    private List<Integer> inorderTraversal2(TreeNode root) {
         if (root != null) {
             inorderTraversal(root.left);
             res.add(root.val);
