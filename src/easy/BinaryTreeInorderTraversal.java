6eb0f71157b548aff5a040f09b2e92732c061dfa
Denis Karpov <karpovdenis.al@gmail.com>
2024-01-31 14:41:38 +0300

    94. Binary Tree Inorder Traversal

---
+++ src/easy/BinaryTreeInorderTraversal.java
@@ -0,0 +1,37 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 94. Binary Tree Inorder Traversal
+public class BinaryTreeInorderTraversal {
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private final List<Integer> res = new ArrayList<>();
+
+    private List<Integer> inorderTraversal(TreeNode root) {
+        if (root != null) {
+            inorderTraversal(root.left);
+            res.add(root.val);
+            inorderTraversal(root.right);
+        }
+        return res;
+    }
+}
