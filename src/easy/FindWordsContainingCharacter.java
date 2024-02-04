84160d90029b029edb13b320be9e38928f9c143a
Denis Karpov <karpovdenis.al@gmail.com>
2024-02-04 23:02:18 +0300

    2942. Find Words Containing Character

---
+++ src/easy/FindWordsContainingCharacter.java
@@ -0,0 +1,21 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 2942. Find Words Containing Character
+public class FindWordsContainingCharacter {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            for (int j = 0; j < word.length(); j++) {
+                if (word.charAt(j) == x) {
+                    arr.add(i);
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+}
