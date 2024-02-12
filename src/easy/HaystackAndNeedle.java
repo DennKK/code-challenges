a5414da877b5765e0d2801af4acefdca96e6dec1
Denis Karpov <karpovdenis.al@gmail.com>
2024-02-12 22:14:56 +0300

    28. Find the Index of the First Occurrence in a String

--- src/easy/HaystackAndNeedle.java
+++ src/easy/HaystackAndNeedle.java
@@ -1,10 +1,10 @@
 package easy;
 
-// Find the Index of the First Occurrence in a String
+// 28. Find the Index of the First Occurrence in a String
 // First solution
 public class HaystackAndNeedle {
     public int strStr(String haystack, String needle) {
-        for (int i = 0;  i < haystack.length() - needle.length() + 1; i++) {
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
             String substr = haystack.substring(i, i + needle.length());
             if (substr.equals(needle)) {
                 return i;
@@ -24,11 +24,11 @@ class Solution2 {
 // Third solution. Without substring function
 class Solution3 {
     public int strStr(String haystack, String needle) {
-        for (int i = 0;  i < haystack.length() - needle.length() + 1; i++) {
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
             if (haystack.charAt(i) == needle.charAt(0)) {
                 boolean allLetterAreCorrect = true;
                 for (int j = 0; j < needle.length(); j++) {
-                    if (!(haystack.charAt(i + j)== needle.charAt(j))) {
+                    if (!(haystack.charAt(i + j) == needle.charAt(j))) {
                         allLetterAreCorrect = false;
                         break;
                     }
