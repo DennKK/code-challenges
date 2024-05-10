c76de1e7c4f7e83f0f645664513b16172b40e569
Denis Karpov <karpovdenis.al@gmail.com>
2024-05-10 22:12:03 +0300

    238. Product of Array Except Self (two solutions)

--- src/medium/ProductOfArrayExceptSelf.java
+++ src/medium/ProductOfArrayExceptSelf.java
@@ -1,6 +1,6 @@
 package medium;
 
-// Product of Array Except Self
+// 238. Product of Array Except Self
 public class ProductOfArrayExceptSelf {
     public static int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
@@ -15,5 +15,40 @@ public class ProductOfArrayExceptSelf {
             postfix = postfix * nums[i];
         }
         return result;
+    }
+}
+
+// One more solution with extra space
+// Maybe it is come to mind easily
+class ProductOfArrayExceptSelf2 {
+    public int[] productExceptSelf(int[] nums) {
+        int product = 1;
+        int len = nums.length;
+        int[] onTheLeft = new int[len];
+        int[] onTheRight = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            product *= nums[i];
+            onTheLeft[i] = product;
+        }
+
+        product = 1;
+        for (int i = len - 1; i >= 0; i--) {
+            product *= nums[i];
+            onTheRight[i] = product;
+        }
+
+        int[] answer = new int[len];
+        for (int i = 0; i < len; i++) {
+            if (i == 0) {
+                answer[i] = onTheRight[i + 1];
+            } else if (i == len - 1) {
+                answer[i] = onTheLeft[i - 1];
+            } else {
+                answer[i] = onTheLeft[i - 1] * onTheRight[i + 1];
+            }
+        }
+
+        return answer;
     }
 }
