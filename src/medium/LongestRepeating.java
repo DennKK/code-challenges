16525505d8e6e6e99ffecd54bcbbf543e4a35b57
Denis Karpov <karpovdenis.al@gmail.com>
2024-06-01 14:41:17 +0300

    424. Longest Repeating Character Replacement

--- src/medium/LongestRepeating.java
+++ src/medium/LongestRepeating.java
@@ -1,5 +1,8 @@
 package medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // 424. Longest Repeating Character Replacement
 public class LongestRepeating {
     public int characterReplacement(String s, int k) {
@@ -15,5 +18,34 @@ public class LongestRepeating {
             res = Math.max(i - j + 1, res);
         }
         return res;
+    }
+}
+
+class LongestRepeating2 {
+    public int characterReplacement(String s, int k) {
+        Map<Character, Integer> count = new HashMap<>();
+        int res = 0;
+
+        int l = 0, maxF = 0;
+        for (int r = 0; r < s.length(); r++) {
+            count.put(s.charAt(r), count.getOrDefault(s.charAt(r), 0) + 1);
+            maxF = Math.max(maxF, maxValueInMap(count));
+
+            if ((r - l + 1) - maxF > k) {
+                count.put(s.charAt(l), count.get(s.charAt(l)) - 1);
+                l++;
+            }
+
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+
+    private int maxValueInMap(Map<Character, Integer> map) {
+        int maxVal = Integer.MIN_VALUE;
+        for (int value : map.values()) {
+            maxVal = Math.max(maxVal, value);
+        }
+        return maxVal;
     }
 }
