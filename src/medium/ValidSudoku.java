cccc25b99bf6764e921abc8609673f2f63cc1ce4
Denis Karpov <karpovdenis.al@gmail.com>
2024-06-03 11:55:44 +0300

    36. Valid Sudoku

--- src/medium/ValidSudoku.java
+++ src/medium/ValidSudoku.java
@@ -1,6 +1,8 @@
 package medium;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 // 36. Valid Sudoku
@@ -14,12 +16,53 @@ public class ValidSudoku {
                 if (number != '.') {
                     if (!seen.add(number + " in row " + i) ||
                             !seen.add(number + " in column " + j) ||
-                            !seen.add(number + " in block " + i / 3 + "-" + j/3)) {
+                            !seen.add(number + " in block " + i / 3 + "-" + j / 3)) {
                         return false;
                     }
                 }
             }
         }
+        return true;
+    }
+}
+
+class ValidSudoku2 {
+    public boolean isValidSudoku(char[][] board) {
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<Integer, Set<Character>> columns = new HashMap<>();
+        Map<String, Set<Character>> boxes = new HashMap<>();
+
+        for (int i = 0; i < 9; i++) {
+            rows.put(i, new HashSet<>());
+            columns.put(i, new HashSet<>());
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                String boxKey = (i / 3) + "-" + (j / 3);
+                if (!boxes.containsKey(boxKey)) {
+                    boxes.put(boxKey, new HashSet<>());
+                }
+
+                if (board[i][j] == '.') {
+                    continue;
+                }
+
+                if (
+                        rows.get(i).contains(board[i][j]) ||
+                                columns.get(j).contains(board[i][j]) ||
+                                boxes.get(boxKey).contains(board[i][j])
+                ) {
+                    return false;
+                }
+
+                rows.get(i).add(board[i][j]);
+                columns.get(j).add(board[i][j]);
+                boxes.get(boxKey).add(board[i][j]);
+            }
+        }
+
         return true;
     }
 }
