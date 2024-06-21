381142701a019c62c17150ead391c0c7f1a039d7
Denis Karpov <karpovdenis.al@gmail.com>
2024-06-21 14:32:41 +0300

    94. Binary Tree Inorder Traversal

--- src/easy/BinaryTreeInorderTraversal.java
+++ src/easy/BinaryTreeInorderTraversal.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 
 // 94. Binary Tree Inorder Traversal
 public class BinaryTreeInorderTraversal {
-    private static class TreeNode {
+    static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -25,8 +25,8 @@ public class BinaryTreeInorderTraversal {
         }
     }
 
-    // Iterative solution
-    private List<Integer> inorderTraversal(TreeNode root) {
+    // First iterative solution
+    private List<Integer> inorderTraversal1(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
         TreeNode curr = root;
@@ -44,15 +44,50 @@ public class BinaryTreeInorderTraversal {
         return res;
     }
 
-    // Recursive solution
-    private final List<Integer> res = new ArrayList<>();
-
+    // Second iterative solution
     private List<Integer> inorderTraversal2(TreeNode root) {
-        if (root != null) {
-            inorderTraversal(root.left);
-            res.add(root.val);
-            inorderTraversal(root.right);
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root == null) {
+            return result;
         }
-        return res;
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode curNode = stack.peek();
+
+            if (curNode.left == null) {
+                result.add(curNode.val);
+                stack.pop();
+                if (!stack.isEmpty()) {
+                    stack.peek().left = null;
+                }
+                if (curNode.right != null) {
+                    stack.push(curNode.right);
+                }
+            } else {
+                stack.push(curNode.left);
+            }
+        }
+
+        return result;
+    }
+
+    // Recursive solution
+    private List<Integer> inorderTraversalRecursive(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+
+        return result;
+    }
+
+    private void inorder(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left, result);
+        result.add(node.val);
+        inorder(node.right, result);
     }
 }
