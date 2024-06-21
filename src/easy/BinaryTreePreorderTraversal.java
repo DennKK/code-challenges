54ffed64544c052e2c37297b5ec12d3045c54b6a
Denis Karpov <karpovdenis.al@gmail.com>
2024-06-21 14:32:54 +0300

    144. Binary Tree Preorder Traversal

--- src/easy/BinaryTreePreorderTraversal.java
+++ src/easy/BinaryTreePreorderTraversal.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 
 // 144. Binary Tree Preorder Traversal
 public class BinaryTreePreorderTraversal {
-    private static class TreeNode {
+    static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -25,6 +25,7 @@ public class BinaryTreePreorderTraversal {
         }
     }
 
+    // Iterative solution
     private List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
@@ -36,10 +37,27 @@ public class BinaryTreePreorderTraversal {
                 stack.push(curr);
                 curr = curr.left;
             }
-            curr = stack.pop();
-            curr = curr.right;
+            curr = stack.pop().right;
         }
 
         return res;
+    }
+
+    // Recursive solution
+    private final List<Integer> res = new ArrayList<>();
+
+    private List<Integer> recursivePreorderTraversal(TreeNode root) {
+        preorder(root);
+        return res;
+    }
+
+    private void preorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        res.add(node.val);
+        preorder(node.left);
+        preorder(node.right);
     }
 }
