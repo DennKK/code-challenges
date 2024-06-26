7ac3a3cb475f4a734047ad07343bb595aa95abff
Denis Karpov <karpovdenis.al@gmail.com>
2024-06-26 11:14:04 +0300

    572. Subtree of Another Tree

--- src/easy/SubtreeOfAnotherTree.java
+++ src/easy/SubtreeOfAnotherTree.java
@@ -21,31 +21,31 @@ public class SubtreeOfAnotherTree {
         }
     }
 
-    // First variation O(n * m)
+    // First variation. Time Complexity: O(n * m)
     private boolean subtree = false;
 
     private boolean isSubtree1(TreeNode root, TreeNode subRoot) {
-        dfs(root, subRoot);
+        dfs1(root, subRoot);
         return subtree;
     }
 
-    private void dfs(TreeNode node, TreeNode subRoot) {
+    private void dfs1(TreeNode node, TreeNode subRoot) {
         if (node == null) {
             return;
         }
 
         if (node.val == subRoot.val) {
-            if (isSame(node, subRoot)) {
+            if (isSame1(node, subRoot)) {
                 subtree = true;
                 return;
             }
         }
 
-        dfs(node.left, subRoot);
-        dfs(node.right, subRoot);
+        dfs1(node.left, subRoot);
+        dfs1(node.right, subRoot);
     }
 
-    private boolean isSame(TreeNode tree1, TreeNode tree2) {
+    private boolean isSame1(TreeNode tree1, TreeNode tree2) {
         if (tree1 == null && tree2 == null) {
             return true;
         }
@@ -58,6 +58,39 @@ public class SubtreeOfAnotherTree {
             return false;
         }
 
-        return isSame(tree1.left, tree2.left) && isSame(tree1.right, tree2.right);
+        return isSame1(tree1.left, tree2.left) && isSame1(tree1.right, tree2.right);
+    }
+
+    // Second variation. Time Complexity also: O(n * m)
+    private boolean isSubtree2(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) {
+            return true;
+        }
+
+        if (root == null) {
+            return false;
+        }
+
+        if (isSame2(root, subRoot)) {
+            return true;
+        }
+
+        return isSubtree2(root.left, subRoot) || isSubtree2(root.right, subRoot);
+    }
+
+    private boolean isSame2(TreeNode tree1, TreeNode tree2) {
+        if (tree1 == null && tree2 == null) {
+            return true;
+        }
+
+        if (tree1 == null || tree2 == null) {
+            return false;
+        }
+
+        if (tree1.val != tree2.val) {
+            return false;
+        }
+
+        return isSame2(tree1.left, tree2.left) && isSame2(tree1.right, tree2.right);
     }
 }
