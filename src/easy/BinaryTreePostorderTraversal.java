03e339a7b22c76b48d0d91a13348380858913bdb
Denis Karpov <karpovdenis.al@gmail.com>
2024-09-24 10:52:50 +0300

    145. Binary Tree Postorder Traversal

--- src/easy/BinaryTreePostorderTraversal.java
+++ src/easy/BinaryTreePostorderTraversal.java
@@ -28,6 +28,37 @@ public class BinaryTreePostorderTraversal {
     // The simplest solution
     private List<Integer> postorderTraversal1(TreeNode root) {
         List<Integer> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        TreeNode curNode = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || curNode != null) {
+            while (curNode != null) {
+                stack.push(curNode);
+                curNode = curNode.left;
+            }
+
+            curNode = stack.pop();
+            if (curNode.right == null) {
+                res.add(curNode.val);
+                curNode = null;
+            } else {
+                TreeNode temp = curNode.right;
+                curNode.right = null;
+                stack.push(curNode);
+                curNode = temp;
+            }
+        }
+
+        return res;
+    }
+
+    // Second solution
+    private List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
         Stack<Boolean> visit = new Stack<>();
         stack.push(root);
@@ -54,39 +85,7 @@ public class BinaryTreePostorderTraversal {
         return res;
     }
 
-    // Second solution
-    private List<Integer> postorderTraversal2(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode curr = root;
-
-        while (curr != null || !stack.isEmpty()) {
-            while (curr != null) {
-                stack.push(curr);
-                curr = curr.left;
-            }
-            curr = stack.pop();
-            if (curr.right == null) {
-                res.add(curr.val);
-                if (!stack.isEmpty()) {
-                    curr = stack.pop();
-                    if (curr.left != null) {
-                        curr.left = null;
-                    } else {
-                        curr.right = null;
-                    }
-                } else {
-                    curr = null;
-                }
-
-            } else {
-                stack.push(curr);
-                curr = curr.right;
-            }
-        }
-        return res;
-    }
-
+    // Recursion solution
     private final List<Integer> res = new ArrayList<>();
 
     private List<Integer> postorderTraversal(TreeNode root) {
